5bb8ef3ee46da7b60175754db8dfa75a0c8a9461
Joe Fong <kapppa.joe@gmail.com>
2021-09-22 09:34:09 +0100

    implement GET /api list all endpoints

--- routes/api.route.js
+++ routes/api.route.js
@@ -1,8 +1,18 @@
 const apiRouter = require("express").Router();
+const listEndpoints = require("express-list-endpoints");
+
 const categoriesRouter = require("./categories.route");
 const reviewsRouter = require("./reviews.route");
 
 apiRouter.use("/categories", categoriesRouter);
 apiRouter.use("/reviews", reviewsRouter);
+
+apiRouter.get("/", getAllEndpoints);
+
+function getAllEndpoints(req, res, next) {
+  const endPoints = listEndpoints(apiRouter);
+  endPoints.forEach((obj) => delete obj.middlewares);
+  res.status(200).send(endPoints);
+}
 
 module.exports = apiRouter;
