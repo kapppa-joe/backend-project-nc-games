0085a994eaaf8e827d8d5ca776a1c6c28c591ed7
Joe Fong <kapppa.joe@gmail.com>
2021-10-18 16:33:57 +0100

    allow cross origin resource shariing

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const apiRouter = require("./routes/api.route");
 const { applyErrorHandlers } = require("./errors");
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
 
